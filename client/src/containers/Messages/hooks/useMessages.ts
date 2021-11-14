8f80b8492986c23bee0b83e811da16d1a9d90688
kstoqnov <kirilstoqnov@gmail.com>
2021-11-14 18:06:56 +0200

    add unmount

--- client/src/containers/Messages/hooks/useMessages.ts
+++ client/src/containers/Messages/hooks/useMessages.ts
@@ -1,15 +1,20 @@
 import { Empty } from "google-protobuf/google/protobuf/empty_pb";
 import { useState, useEffect } from "react";
 import { MessengerClient } from "messenger/MessengerServiceClientPb";
+import { ClientReadableStream } from "grpc-web";
+import { MessageResponse } from "messenger/messenger_pb";
 
 export const useMessages = (client: MessengerClient) => {
   const [messages, setMessages] = useState<string[]>([]);
 
   useEffect(() => {
-    const stream$ = client.getMessages(new Empty());
-    stream$.on("data", m => {
+    const stream = client.getMessages(new Empty()) as ClientReadableStream<MessageResponse>
+    stream.on("data", m => {
       setMessages(state => [...state, m.getMessage()]);
     });
+
+    return () => stream.cancel()
+
   }, [client]);
 
   return {
